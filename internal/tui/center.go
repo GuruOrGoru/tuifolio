d8945bc3e087aaf783e78773426507c7ebc6bdb1
GuruOrGoru <siddharthadhakall3722@gmail.com>
2025-10-17 10:29:24 +0545

    Fixed centering issues

--- internal/tui/center.go
+++ internal/tui/center.go
@@ -6,11 +6,7 @@ func CenterSquareWithContent(
 	appWidth, appHeight, outerWidth, outerHeight, innerWidth, innerHeight int,
 	content string,
 ) string {
-	outerStyle := CenterOuterStyle.
-		Width(outerWidth).
-		Height(outerHeight).
-		Margin((appHeight-outerHeight)/2, (appWidth-outerWidth)/2)
-
 	innerRendered := lipgloss.Place(outerWidth, outerHeight, lipgloss.Center, lipgloss.Center, content)
-	return outerStyle.Render(innerRendered)
+	outerRendered := CenterOuterStyle.Width(outerWidth).Height(outerHeight).Render(innerRendered)
+	return lipgloss.Place(appWidth, appHeight, lipgloss.Center, lipgloss.Center, outerRendered)
 }
